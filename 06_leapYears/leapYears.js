503acafa2e00ac54b207251069d9ae627835631e
sandi <satrianindhita6@gmailcom>
2024-08-20 06:51:47 +0700

    feat: :fire: Assigment 6 Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
-};
+    const isFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isFourHundred = year % 400 === 0;
+
+    if (isFour && (!isCentury || isFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
