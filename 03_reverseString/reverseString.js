8404bf2ec2f5608071b0070dfb115bb3bfc0268e
sandi <satrianindhita6@gmailcom>
2024-08-20 04:37:50 +0700

    feat: :art: Assignment 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    // Split the word into an array of characters
+    let charArray = word.split('');
 
+    // Reverse the array of characters
+    let reversedArray = charArray.reverse();
+
+    // Join the reversed array back into a string
+    let reversedWord = reversedArray.join('');
+
+    return reversedWord;
 };
 
 // Do not edit below this line
