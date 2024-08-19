9957c646a4f5220fd88b669776d4c6696858456e
sandi <satrianindhita6@gmailcom>
2024-08-20 04:25:12 +0700

    feat: :zap: Assignment 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    let string = '';
 
+    // Error check should be outside the loop
+    if (times < 0 || times !== parseInt(times)) {
+        return "ERROR";
+    }
+
+    // Loop to repeat the string
+    for (let index = 0; index < times; index++) {
+        string += word;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
