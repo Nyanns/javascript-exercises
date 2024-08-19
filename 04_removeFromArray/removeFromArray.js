5d03a13e82e17de11d09ce2a5a296095b8b39c72
sandi <satrianindhita6@gmailcom>
2024-08-20 05:31:54 +0700

    feat: :art: assignment 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    // Create a new empty array to hold the filtered items
+    const newArray = [];
+
+    // Loop through each item in the original array
+    for (let i = 0; i < array.length; i++) {
+        // Check if the current item is NOT in the args array
+        // If it's not in the args array, add it to the newArray
+        if (!(args.includes(array[i]))) {
+            newArray.push(array[i]);
+        }
+    }
+
+    // Return the new array that has the items we want
+    return newArray;
 };
 
 // Do not edit below this line
