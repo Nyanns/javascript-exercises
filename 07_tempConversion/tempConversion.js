d3667c4541998ddbc366289d7e7327a87d36761e
sandi <satrianindhita6@gmailcom>
2024-08-20 22:34:14 +0700

    feat: :fire: Assignment 6 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value) {
+  let celsius = 0;
+  let convert = ((value - 32) * 5 / 9);
+  convert = Math.round(convert * 10) / 10;
+  celsius = convert;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (value) {
+  const baseFahrenheit = 32;
+  let convert = ((value * 9) / 5) + 32;
+  convert = Math.round(convert * 10) / 10;
+  let fahrenheit = convert;
+  return fahrenheit;
 };
 
 // Do not edit below this line
