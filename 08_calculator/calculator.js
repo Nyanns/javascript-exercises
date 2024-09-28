4639d7326da8c64a8174d73501711fddcf8cae39
sandi <satrianindhita6@gmailcom>
2024-09-28 23:23:57 +0700

    feat: :white_check_mark: Calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => {
+    return total + current
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  result = 1;
+  for (let index = 0; index < exponent; index++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let index = 0; index < n; index++) {
+      result = result * (n - index);
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
