4d8eff54fc40f89a92d39f71217aadc83589f41c
sandi <satrianindhita6@gmailcom>
2024-09-29 17:14:59 +0700

    feat: :zap: solution 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    // Menggunakan .map() untuk mengambil properti title dari setiap objek book
+    const titles = books.map((book) => book.title);
+    return titles; // Mengembalikan array berisi judul buku
 };
 
 // Do not edit below this line
